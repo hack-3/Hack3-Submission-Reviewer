18f6c40c217411ad1ef37e5ae15c59d1d986cc3c
Oof <chris.li2005@gmail.com>
2021-01-15 16:45:37 -0500

    fixed some stuff up

--- hack3/Config.py
+++ hack3/Config.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Config:
     _instance = None
 
@@ -7,10 +8,17 @@ class Config:
         if cls._instance is None:
             cls._instance = super(Config, cls).__new__(cls)
 
-            with open("../storage.json", "r") as f:
+            with open("hack3/storage.json", "r") as f:
                 data = json.loads(f.read())
 
             cls.user = data["user"]
             cls.password = data["password"]
             cls.host = data["host"]
         return cls._instance
+
+
+if (__name__ == "__main__"):
+    config = Config()
+    print(config.user)
+    print(config.password)
+    print(config.host)
---
+++ test.py
@@ -0,0 +1,3 @@
+from hack3 import mysql_functions
+
+mysql_functions.store_urls_batch(ending_page=1)
