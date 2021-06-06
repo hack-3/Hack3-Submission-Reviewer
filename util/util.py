fb7bbebff06cb142c4ac9172539554a8cb1ebaa5
Oof <chris.li2005@gmail.com>
2021-06-06 00:15:47 -0400

    create most of the backbone

--- util/util.py
+++ util/util.py
@@ -1,6 +1,8 @@
-from typing import List, Iterable
+import re
+from typing import List
 from fuzzywuzzy import fuzz
 import tlsh
+from util import mysql_datatypes as DataType, mysql_util, webscraping_utils
 
 
 def get_hash(string: str) -> str:
@@ -9,7 +11,7 @@ def get_hash(string: str) -> str:
 
 
 def parse_file_name(file_name: str) -> List[str]:
-    temp = file_name.rsplit(",", 1)
+    temp = file_name.rsplit(".", 1)
 
     if len(temp) == 1:
         temp.insert(0, "")
@@ -28,3 +30,49 @@ def compare_hashes(hash1: str, hash2: str) -> bool:
     else:
         ratio = fuzz.ratio(hash1, hash2)
         return ratio > 70  # TODO - find good ratio here
+
+
+def store_projects(starting_page: int = 1, ending_page: int = 10):
+    if starting_page <= 0:
+        starting_page = 1
+
+    while starting_page <= ending_page:
+        projects = webscraping_utils.get_new_projects(starting_page)
+
+        for project in projects:
+            print(f"Storing project {project}")
+
+            html = webscraping_utils.get_html(project)
+
+            desc_hash = get_hash(webscraping_utils.get_project_description("", html=html))
+            github_links = ",".join(webscraping_utils.get_project_sources("", html=html))
+
+            mysql_util.store_devpost_project(project, github_links, desc_hash)
+
+        starting_page += 1
+
+
+def store_project_sources():
+    unadded_projects = mysql_util.get_values("projects", "devpostUrl", "githubSources", restrictions=["added = FALSE"])
+
+    for source in unadded_projects:
+        print(f"Storing project {source[0]}")
+        links = source[1].split(",")
+
+        for link in links:
+            if link == "":
+                continue
+
+            user, repo = re.findall("^https://github.com/([\w-]+)/([\w-]+)", link)[0]
+            file_links = webscraping_utils.get_github_files(user, repo)
+
+            for file in file_links:
+                file_hash = get_hash(webscraping_utils.get_file_content(file[1]))
+
+                if file_hash == "":
+                    return
+
+                name, ext = parse_file_name(file[0])
+                mysql_util.store_file(source[0], file_hash, name, ext)
+
+        mysql_util.update_table("projects", restrictions=[f"devpostUrl='{source[0]}'"], added=DataType.Bool(True))
