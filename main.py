c0bec7b49d962bd2d93b5785c876da8e8800532c
Oof <chris.li2005@gmail.com>
2021-06-06 19:59:32 -0400

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+from util import util
+
+a = input("What would you like to do? (add projects, add files, check project): ")
+
+if a == "add projects":
+    util.store_projects_batch(ending_page=1)
+elif a == "add files":
+    util.store_project_sources()
+elif a == "check project":
+    util.s
